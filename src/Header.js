8ee3dd5f0bfcd67922c07b0b4c8c5d7aefc52c3b
arnab roy <theachieverarnab@gmail.com>
2021-02-28 19:46:26 +0530

    Inital Commit

---
+++ src/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./Header.css";
+import SearchIcon from '@material-ui/icons/Search';
+import LanguageIcon from '@material-ui/icons/Language';
+import {Avatar} from "@material-ui/core";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+        <div className="header">
+            <Link to="/"><img className="header_icon" src="https://1000logos.net/wp-content/uploads/2017/08/Airbnb-logo.jpg" alt="logo"/></Link>
+            
+            <div className="header_center">
+                <input type="text"/>
+                <SearchIcon/>
+            </div>
+            <div className="header_right">
+                <p>Become a host</p>
+                <LanguageIcon/>
+                <ExpandMoreIcon/>
+                <Avatar/>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
